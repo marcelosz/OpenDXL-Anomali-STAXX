62d4057780de00e2dfcb3d40fc8acfafc5bc85e2
Marcelo Souza <marcelo@marcelosouza.com>
2017-12-07 16:41:56 -0200

    Working version (0.1).

--- staxx_exporter.py
+++ staxx_exporter.py
@@ -47,10 +47,11 @@ def create_arg_parser():
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog=textwrap.dedent(epilog))
     parser.add_argument("filter_query", help="Query used to filter desired observables (confidence, type, time window, ...).", metavar="FILTER_QUERY")
-    parser.add_argument("-c", "--config_file", help="Configuration file.", default="/etc/opendxl-anomali-staxx/client.conf")
+    parser.add_argument("-c", "--configfile", help="Configuration file.", default="/etc/opendxl-anomali-staxx/client.conf")
     parser.add_argument("-d", "--dryrun", help="Export observables from STAXX without generating DXL messages.", action='store_true', default=False)
     parser.add_argument("-l", "--loglevel", help="Logging level (DEBUG, INFO or ERROR).", default="INFO")
     parser.add_argument("-p", "--pprint", help="Pretty print exported observables to STDOUT.", action='store_true', default=False)
+    parser.add_argument("-s", "--singleshot", help="Single shot mode (will not keep polling STAXX server).", action='store_true', default=False)
     parser.add_argument("-t", "--time", help="Polling time (in seconds).", default=60)    
 
     return parser
@@ -127,14 +128,18 @@ def get_staxx_observables(address, port, token, query):
         logger.debug("JSON response ok!")
         return False, json_obj
 
-def publish_dxl_message(payload):
+def is_observable_type_listed(values, searchFor):
     """
-    Publish a message to the DXL fabric.
+    TODO
 
     Returns:
         TODO
     """
-    return payload
+    for k in values:
+        for v in values[k]:
+            if searchFor in v:
+                return k
+    return None
 
 ###############################################################################
 
@@ -149,7 +154,7 @@ def main(argv):
     set_logging_level(logging.getLogger("urllib3"), args.loglevel)
     # read cfg file
     try:
-        config = ConfigObj(args.config_file, raise_errors=True, file_error=True)
+        config = ConfigObj(args.configfile, raise_errors=True, file_error=True)
     except:
         # TODO - enhance error handling here 
         logger.error("Could not parse config file!")
@@ -158,7 +163,7 @@ def main(argv):
     #
     # get token
     #
-    # TODO - handle HTTPS nicely 
+    # TODO - handle HTTPS nicely
     urllib3.disable_warnings()
     token = get_staxx_token(config['STAXX']['Address'],config['STAXX']['Port'],
                             config['STAXX']['User'],config['STAXX']['Password'])
@@ -169,12 +174,17 @@ def main(argv):
     #
     # DXL initialization
     #
-    # TODO - enhance error handling here 
+    # TODO - enhance error handling here
     if not args.dryrun:
         # DxlClientConfig from DXL configuration file
         logger.info("Loading DXL config from: %s", config['DXL']['Config'])
         dxl_config = DxlClientConfig.create_dxl_config_from_file(config['DXL']['Config'])
-
+        #
+        # build the topics
+        #
+        obs_types = config['Observable Types']
+        logger.debug("Observable types: %s", obs_types)
+        obs_topics = config['Observable Topics']
     #
     # timed loop (synchronous "obs export / msg publish")
     #
@@ -182,41 +192,64 @@ def main(argv):
         #
         # export observables into JSON object
         # 
-        req_error, json_observables = get_staxx_observables(config['STAXX']['Address'],config['STAXX']['Port'],
-                                                 token, args.filter_query)
+        req_error, json_obs = get_staxx_observables(config['STAXX']['Address'],config['STAXX']['Port'],
+                                                    token, args.filter_query)
         if req_error:
-            logger.error("Failure exporting observables on this polling cycle. Sleeping until next.")
-            time.sleep(args.time)
-            logger.info("New polling cycle...")
-            continue
-        if json_observables:
-            logger.info("{0} observable(s) exported from Anomali STAXX.".format(len(json_observables)))
+            logger.error("Failure exporting observables.")
+            if not args.singleshot:
+                logger.info("Sleeping until next polling cycle...\n")
+                time.sleep(args.time)
+                logger.info("New polling cycle.")
+                continue
+        if json_obs:
+            logger.info("{0} observable(s) exported from Anomali STAXX.".format(len(json_obs)))
             if args.pprint: 
                 logger.info("Printing observables to STDOUT...")
-                print json.dumps(json_observables, indent=2,sort_keys=False)
+                print json.dumps(json_obs, indent=2,sort_keys=False)
             if not args.dryrun:
                 #
-                # Publish the observables as events
+                # Connect to DXL and publish the observables as events
                 #
                 try:
                     with DxlClient(dxl_config) as dxl_client:
                         # Connect to DXL Broker
                         logger.info("Connecting to DXL broker...")
                         dxl_client.connect()
-                        logger.info("Publishing events...")
-                        dxl_event = Event(DXL_MSG_ROOT_TOPIC)
-                        dxl_event.payload = str("Whatever").encode()
-                        dxl_client.send_event(dxl_event)
+                        # TODO - handle possible connection errors
+                        logger.info("Filtering observables and publishing events...")
+                        count = 0
+                        for ob in json_obs:
+                            key = is_observable_type_listed(obs_types, ob['itype'])
+                            if key:
+                                count += 1
+                                logger.debug("Publishing message for observable (itype: %s, topic: %s).", ob['itype'], obs_topics[key])
+                                dxl_event = Event(obs_topics[key])
+                                payload_str = json.dumps(ob)
+                                logger.debug("Msg payload: %s", payload_str)
+                                dxl_event.payload = str(payload_str).encode()
+                                dxl_client.send_event(dxl_event)
+                            else:
+                                logger.info("Observable not published (itype: %s not listed).", ob['itype'])
+                        logger.info("%s event(s) published to DXL fabric.", count)
                 except Exception as e:
                     logger.error("Could not initialize OpenDXL client ({0}).".format(e.message))
                     exit(1)
         else:
             logger.info("No observable exported from Anomali STAXX.")    
         
-        # wait for next cycle
-        logger.info("Sleeping until next polling cycle.")
-        time.sleep(args.time)
-        logger.info("New polling cycle...")
+        # wait for next cycle (if not single shot mode)
+        if args.singleshot:
+            logger.info("Exiting (single shot mode).")
+            exit(0)
+        else:
+            logger.info("Sleeping until next polling cycle...\n")
+            time.sleep(args.time)
+            logger.info("New polling cycle.")
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        # TODO - gracefully exit
+        logger.info("Caught keyboard interrupt signal. Exiting...")
+        exit(0)
